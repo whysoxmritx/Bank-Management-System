cab4a15d4607911afddedff5a9c470f93ce7c650
Amrita Pandey <amritap7238@gmail.com>
2025-07-08 20:14:23 +0530

    push

--- BalanceEnquiry.java
+++ BalanceEnquiry.java
@@ -11,11 +11,30 @@ public class BalanceEnquiry {
         h.getContentPane().setBackground(new Color(30,30,60));
         ImageIcon ig=new ImageIcon("logo.jpg");
         h.setIconImage(ig.getImage());
-        JLabel ac=new JLabel("A/C no.");
+        JLabel ac=new JLabel("A/C no.:");
         h.add(ac);
         ac.setForeground(Color.lightGray);
-        ac.setFont(new Font(Font.MONOSPACED,2,17));
-        ac.setBounds(60,60,100,40);
+        ac.setFont(new Font(Font.MONOSPACED,1,20));
+        ac.setBounds(80,130,100,40);
+        JTextField bal=new JTextField();
+        h.add(bal);
+        bal.setBounds(220,130,130,30);
+        JLabel phn=new JLabel("Phone No.:");
+        h.add(phn);
+        phn.setForeground(Color.lightGray);
+        phn.setFont(new Font(Font.MONOSPACED,1,20));
+        phn.setBounds(80,180,130,40);
+        JTextField ph=new JTextField();
+        h.add(ph);
+        ph.setBounds(220,180,130,30);
+        JLabel branch=new JLabel("Branch:");
+        h.add(branch);
+        branch.setForeground(Color.lightGray);
+        branch.setFont(new Font(Font.MONOSPACED,1,20));
+        branch.setBounds(80,230,100,40);
+        JTextField brn=new JTextField();
+        h.add(brn);
+        brn.setBounds(220,230,130,30);
         h.setVisible(true);
     }
     public static void main(String[] args){
